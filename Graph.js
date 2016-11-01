7ff2ab2eeda20916965b54d4168caeb9ebc007e2
Vijay Siva <vijay_732@hotmail.com>
2016-11-01 16:14:04 -0400

    basic functions complete

--- Graph.js
+++ Graph.js
@@ -1,36 +1,67 @@
+/*
+ *
+ */ 
 class Vertex {
   constructor(name) {
     this.name = name; 
     this.edges = []; 
   }
 
-  equals(v) {
-
+  /*
+   *  Add an edge connecting this vertex to the given vertex v to the adjacency list. 
+   *  If this edge already exists, the adjacency list is left unchanged. 
+  */
+  addEdge(v) {
+    if (!this.connectedTo(v)) {
+      this.edges.push(new Edge(v));
+    } 
+  }
+  
+  /*
+   *  Remove the edge connected to the given vertex from this vertex's adjacency list. 
+   *  If there isn't an edge connecting this vertex and v, the adjacency list is left 
+   *  unchanged. 
+   */ 
+  removeEdge(v) {
+    for (var i = 0; i < this.edges.length; i++) {
+      if (this.edges[i].neighbor === v) {
+        this.edges.splice(i, 1); 
+      }
+    }
   }
 
-  addEdge(v) {
-    this.edges.push(new Edge(v)); 
+  /*
+   *  Return true iff this vertex has an edge connected to the given vertex v. 
+   */
+  connectedTo(v) {
+    for (var i = 0; i < this.edges.length; i++) {
+      if (this.edges[i].neighbor === v) {
+        return true; 
+      }
+    }
+    return false; 
   }
 };
 
+/*
+ *  
+ */ 
 class Edge {
   constructor(neighbor) {
-    this.weight = 0; 
-    this.neigbor = neighbor; 
-  }
-
-  equals(e) {
+    this.weight = arguments[1] || 0; 
+    this.neighbor = neighbor; 
   }
 }
 
+/*
+ *
+ */ 
 class Graph {
   constructor() {
     this.vertices = [];
     this.isDirected = false; 
   }
 
-  equals(other) {
-  }
 
  /*
   *  Add a vertex with the given name to the graph.
@@ -42,15 +73,25 @@ class Graph {
   }
 
  /*
-  *  Remove a vertex with the given name from the graph. If no vertex exists
-  *   with the given name, the graph will remain unchanged.
+  *  Remove the given vertex from the graph. If the given vertex is not 
+  *  a member of this graph, the graph is left unchanged. 
   *
-  *   @param {string} name The name of the vertex to be removed.
+  *   @param {Vertex} v The vertex to be removed 
   */
-  removeVertex(name) {
-
+  removeVertex(v) {
+    for (var i = 0; i < this.vertices.length; i++) {
+      if (this.vertices[i] === v) {
+        this.vertices.splice(i, 1); 
+        return; 
+      }
+    }
   }
+  
 
+  /*
+   *  Return the vertex in this graph with the given name. If no vertex exists 
+   *  in this graph with the given name, return null. 
+   */
   getVertex(name) {
     for (var i = 0; i < this.vertices.length; i++) {
       if (this.vertices[i].name === name) {
@@ -65,15 +106,28 @@ class Graph {
    *
    *   @param {Vertex} v1
    *   @param {Vertex} v2
-   *  @param {number} weight
+   *   @param {number} weight
    */
   addEdge(v1, v2) {
-    v1.edges.push(new Edge(v2, weight));
+    v1.addEdge(v2); 
 
     // If undirected graph, add the other direction as well
     if (!this.isDirected) {
-      v2.edges.push(new Edge(v1, weight))
+      v2.addEdge(v1)
     }
+  }
+
+  /*
+   *  Return true iff there exists an edge between the two given vertices in this graph. 
+   *  Otherwise return false. 
+   */
+  hasEdge(v1, v2) {
+    for (var i = 0; i < v1.edges.length; i++) {
+      if (v1.edges[i].neighbor == v2) {
+        return true
+      }
+    }
+    return false; 
   }
 
   /*
@@ -84,21 +138,11 @@ class Graph {
    *  @param {Vertex} v2
    */
   removeEdge(v1, v2) {
-    for (i = 0; i < v1.edges.length; i++) {
-      if (v1.edges[i].neigbor === v2) {
-        v1.edges.splice(i, 1);
-        break;
-      }
-    }
+    v1.removeEdge(v2); 
 
     // If graph is undirected, remove the other direction
     if(!this.isDirected) {
-      for (i = 0; i < v2.edges.length; i++) {
-        if (v2.edges[i].neigbor === v1) {
-          v2.edges.splice(i, 1);
-          return;
-        }
-      }
+      v2.removeEdge(v1); 
     }
   }  
 
@@ -142,7 +186,9 @@ class Graph {
 g = new Graph(); 
 g.addVertex("a");
 g.addVertex("b"); 
-console.log(g.vertices); 
-console.log(g.getVertex("a")); 
-console.log(g.getVertex("c")); 
-g.removeVertex("b"); 
+a = g.vertices[0];
+b = g.vertices[1]; 
+g.addEdge(a, b);
+console.log(g.hasEdge(a, b));
+g.removeEdge(b, a); 
+console.log(g.hasEdge(a, b));
